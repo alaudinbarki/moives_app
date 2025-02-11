04e5390b3c062bc2e41e7aeed3a376a6056fd347
Ala ud Din <72483032+alaudinbarki@users.noreply.github.com>
2025-02-11 15:07:17 +0500

    initailize app

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+class AppColors {
+  AppColors._();
+  static const purpleGray = Color(0xFF2E2739);
+  static const grayishWhite = Color(0xFFF6F6FA);
+  static const grayishPurple = Color(0xFF827D88);
+  static const lightBlue = Color(0xFF61C3F2);
+  static const cloudyLavender = Color(0xFFDBDBDF);
+  static const aquaGreen = Color(0xFF15D2BC);
+  static const pink = Color(0xFFE26CA5);
+  static const bluePurple = Color(0xFF564CA3);
+  static const goldenYellow = Color(0xFFCD9D0F);
+}
